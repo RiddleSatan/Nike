8a0b8585bbe565bfac41e68d566b16c8166b958e
Om Pandey <ompandey007j@gmail.com>
2023-12-13 20:25:40 +0530

    Navbar changes

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -8,12 +8,12 @@ const Header = () => {
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
           <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+            <img src="https://1000logos.net/wp-content/uploads/2021/11/Nike-Logo.png" className="h-14 invert w-32" alt="Flowbite Logo" />
+            
           </Link>
           <div className="flex items-center space-x-6 rtl:space-x-reverse">
-            <a href="tel:5541251234" className="text-sm text-gray-500 dark:text-white hover:underline">(555) 412-1234</a>
-            <Link to="/login" className="text-sm text-blue-600 dark:text-blue-500 hover:underline">Login</Link>
+            
+          <Link  class="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">Login</Link>
           </div>
         </div>
       </nav>
