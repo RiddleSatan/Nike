1807154da830bb8fd2fb163ca19040803543e2e0
Om Pandey <ompandey007j@gmail.com>
2023-12-14 18:27:21 +0530

    Home Page Updated

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,20 +8,20 @@ import { FaFacebook } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className='w-full h-[60vh] bg-black flex justify-evenly items-center'>
-        <div className='w-80 h-80 bg-black text-white flex-col  justify-self-start color   leading-6 '>
+    <div className='w-full h-[60vh] border-t-2 bg-[#111827] flex justify-evenly items-center'>
+        <div className='w-80 h-80 bg-[#111827] text-white flex-col  justify-self-start color   leading-6 '>
             <img className='w-56 h-24 invert  ' src="https://1000logos.net/wp-content/uploads/2021/11/Nike-Logo.png" alt="" />
             <h1>Get shoes ready for the new term at your</h1>
             <h1>nearest Nike store. Find Your perfect Size In</h1>
             <h1>Store. Get Rewards </h1>
-            <div className='w-full h-10 bg-black mt-7 flex justify-self-start gap-4'>
+            <div className='w-full h-10 bg-[#111827] mt-7 flex justify-self-start gap-4'>
                 <FaFacebook className='text-xl'/>
                 <FaTwitter  className='text-xl'/>
                 <FaInstagram className='text-xl'/>
             </div>
             <h1 className='mt-20'><span>&copy;</span>Copyright. All rights reserved.</h1>
         </div>
-        <div className='w-60 h-80 bg-black flex flex-col  gap-3 text-white '>
+        <div className='w-60 h-80 bg-[#111827] flex flex-col  gap-3 text-white '>
             <h1 className='mb-5 font-semibold text-4xl tracking-wider'>Poducts</h1>
             <h2 className='text-xl  font-medium tracking-wider hover:text-gray-500 transition-all'>Air Force 1</h2>
             <h2 className='text-xl  font-medium tracking-wider hover:text-gray-500 transition-all'>Air Max 1</h2>
@@ -30,7 +30,7 @@ const Footer = () => {
             <h2 className='text-xl  font-medium tracking-wider hover:text-gray-500 transition-all'>Nike Waffle Racer</h2>
             <h2 className='text-xl  font-medium tracking-wider hover:text-gray-500 transition-all'>Nike Cortez</h2>
         </div>
-        <div className='w-60 h-80 bg-black flex flex-col  gap-3 text-white '>
+        <div className='w-60 h-80 bg-[#111827] flex flex-col  gap-3 text-white '>
             <h1 className='mb-5 font-semibold text-4xl tracking-wider'>Help</h1>
             <h2 className='text-xl  font-medium tracking-wider hover:text-gray-500 transition-all'>About us</h2>
             <h2 className='text-xl  font-medium tracking-wider hover:text-gray-500 transition-all'>FAQs </h2>
@@ -38,7 +38,7 @@ const Footer = () => {
             <h2 className='text-xl  font-medium tracking-wider hover:text-gray-500 transition-all'>Privacy policy</h2>
             <h2 className='text-xl  font-medium tracking-wider hover:text-gray-500 transition-all'>Payment policy</h2>
         </div>
-        <div className='w-60 h-80 bg-black flex flex-col  gap-3 text-white '>
+        <div className='w-60 h-80 bg-[#111827] flex flex-col  gap-3 text-white '>
             <h1 className='mb-5 font-semibold text-4xl tracking-wider'>Get in touch</h1>
             <h2 className='text-xl  font-medium tracking-wider hover:text-gray-500 transition-all'>customer@nike.com</h2>
             <h2 className='text-xl  font-medium tracking-wider hover:text-gray-500 transition-all'>+92554862354</h2>
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='flex justify-center'>Home</div>
+    <><div class="content pl-6 bg-black ">
+      <div class="container flex bg-black flex-col md:flex-row items-center justify-between gap-8 md:gap-0 min-h-screen md:min-h-auto">
+        <div class="info text-center bg-black md:text-left mb-8 md:mb-0">
+          <h1 class="text-white text-4xl md:text-6xl">Looking For Inspiration</h1>
+          <p class="text-white text-base md:text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus odit nihil ullam nesciunt quidem iste, Repellendus odit nihil</p>
+          <button class="btn mt-8 px-6 py-3 rounded-lg text-white bg-purple-600 cursor-pointer">Button name</button>
+        </div>
+        <div class="image pr-6 ">
+          <img  src="https://rukminim2.flixcart.com/image/850/1000/xif0q/shoe/m/m/v/-original-imagg7szgcmfgauh.jpeg?q=90" alt="Image" class="max-w-md rounded-md "/>
+        </div>
+      </div>
+    </div></>
   )
 }
 
